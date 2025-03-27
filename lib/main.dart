befa4a4d198b7cd3cc96d54fad839f008f30a106
Wilson Abad <abadskie19@gmail.com>
2025-03-28 01:20:05 +0800

    register provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_experiment/app.dart';
+import 'package:flutter_experiment/providers/experiment_copy_with_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyMaterialApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ExperimentCopyWithProvider()),
+      ],
+      child: MyMaterialApp(),
+    ),
+  );
 }
