db18d73682e3741b0a17f90fa6568b8cabcec268
Wilson Abad <wilsonabad@pangga.ph>
2025-05-10 03:33:10 +0800

    update

--- lib/pages/experiment_onboarding_page.dart
+++ lib/pages/experiment_onboarding_page.dart
@@ -89,24 +89,24 @@ class ExperimentOnBoardingPageState extends State<ExperimentOnBoardingPage> {
       //   ),
       // ),
       pages: [
-        PageViewModel(
-          title: "Fractional shares",
-          body:
-              "Instead of having to buy an entire share, invest any amount you want.",
-          decoration: pageDecoration,
-        ),
-        PageViewModel(
-          title: "Learn as you go",
-          body:
-              "Download the Stockpile app and master the market with our mini-lesson.",
-          decoration: pageDecoration,
-        ),
-        PageViewModel(
-          title: "Kids and teens",
-          body:
-              "Kids and teens can track their stocks 24/7 and place trades that you approve.",
-          decoration: pageDecoration,
-        ),
+        // PageViewModel(
+        //   title: "Fractional shares",
+        //   body:
+        //       "Instead of having to buy an entire share, invest any amount you want.",
+        //   decoration: pageDecoration,
+        // ),
+        // PageViewModel(
+        //   title: "Learn as you go",
+        //   body:
+        //       "Download the Stockpile app and master the market with our mini-lesson.",
+        //   decoration: pageDecoration,
+        // ),
+        // PageViewModel(
+        //   title: "Kids and teens",
+        //   body:
+        //       "Kids and teens can track their stocks 24/7 and place trades that you approve.",
+        //   decoration: pageDecoration,
+        // ),
         PageViewModel(
           title: "Full Screen Page",
           body:
