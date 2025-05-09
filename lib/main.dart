fe855d7766da6bfb35ce4fb13fc33f94b71e95fd
Wilson Abad <wilsonabad@pangga.ph>
2025-05-10 04:01:06 +0800

    inject the service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_experiment/app.dart';
+import 'package:flutter_experiment/core/service_locator.dart';
+import 'package:flutter_experiment/domain/interfaces/services/i_internet_connectivity_service.dart';
 import 'package:flutter_experiment/providers/experiment_copy_with_provider.dart';
+import 'package:flutter_experiment/providers/internet_connectivity_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupLocator();
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ExperimentCopyWithProvider()),
+        ChangeNotifierProvider(
+          create:
+              (_) => InternetConnectivityProvider(
+                locator<IInternetConnectivityService>(),
+              ),
+        ),
       ],
       child: MyMaterialApp(),
     ),
